492edd9649b7e0b064a08520671c17fedcd35d61
Deva_B.D.M. <dawitberhandm@gmail.com>
2024-10-04 19:21:36 +0300

    add: application name , organization name and domain and other

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
+
+    QGuiApplication::setApplicationDisplayName("Woldia University");
+
+    QGuiApplication::setApplicationName("WLDU");
+    QGuiApplication::setOrganizationName("DevaBDM");
+    QGuiApplication::setApplicationVersion("0.0.1");
+    QGuiApplication::setOrganizationDomain("org");
 
     QQmlApplicationEngine engine;
     QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+        []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
     engine.loadFromModule("WLDU", "Main");
 
     return app.exec();
